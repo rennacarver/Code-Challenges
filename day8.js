e6510b7f393413d6ca1496c159f5b56c826b2eff
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-01-08 21:32:56 -0800

    challenge 8

---
+++ day8.js
@@ -0,0 +1,7 @@
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return args.reduce((a, b) => Math.min(a, b))
+  }
+}
+
+https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
