e2a4fa01faab3c4d26d1174ae7e515a07201f87e
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-01-09 23:40:27 -0800

    challenge 9

---
+++ day9.js
@@ -0,0 +1,10 @@
+function doubleChar(str) {
+  let double = ''
+  for (let k in str){
+      double += str[k]
+      double += str[k]
+  }
+  return double
+}
+
+https://www.codewars.com/kata/56b1f01c247c01db92000076/train/javascript
