a98940ccf5a74cef1c92385cfbd428f074078861
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-02-07 10:24:41 -0800

    challenge 39

--- day38.js
+++ day38.js
@@ -8,3 +8,14 @@ var isSquare = function(n){
 }
 
 https://www.codewars.com/kata/54c27a33fb7da0db0100040e/train/javascript
+
+/*less silly solutions
+
+function isSquare(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
+
+const isSquare = n => Number.isInteger(Math.sqrt(n));
+
+
+*/
