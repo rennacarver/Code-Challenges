c4346cf624f0bf540c2f1a76ba7791d7319609a3
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2024-02-15 11:37:58 -0800

    challenge 47

---
+++ day47-twosum.js
@@ -0,0 +1,30 @@
+var twoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if ( i !== j){
+                if(nums[i] + nums[j] == target){
+                    return [i, j]
+                }
+            }
+        }
+    }
+};
+
+//https://leetcode.com/problems/two-sum/
+
+/*Fastest implementation (Java)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+        return null;
+    }
+}
+*/
