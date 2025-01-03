5168b8e1ad13ee2b24ce6cf9469570efe3356353
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-01-02 17:53:17 -0800

    challenge 2

---
+++ NeetCode 250/challenge 2.js
@@ -0,0 +1,17 @@
+class Solution {
+  /**
+   * @param {number[]} nums
+   * @return {boolean}
+   */
+  hasDuplicate(nums) {
+    let object = {}
+
+    for (let k in nums) {
+      if (object[nums[k]]) {
+        return true
+      } else object[nums[k]] = 1
+    }
+    console.log(object)
+    return false
+  }
+}
