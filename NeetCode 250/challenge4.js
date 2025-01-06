e9ba9d13526ecd4497ed2a1ab26e45361656abaf
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-01-06 11:26:02 -0800

    challenge 4 continued

--- NeetCode 250/challenge4.js
+++ NeetCode 250/challenge4.js
@@ -1,7 +1,24 @@
+//brainstorming
+//dictionary with value and its index
+// {
+//  3: 0
+//  4: 1
+//  5: 2
+//  6: 3
+//}
+
 function twoSum(nums, target) {
-  //dictionary with value and its index
-  // {
-  //}
+  let hmap = {}
+  for (k in nums) {
+    console.log(nums[k])
+
+    if (hmap[target - nums[k]]) return [hmap[target - nums[k]], k]
+  }
+
+  hmap[nums[k]] = k
+
+  console.log(hmap)
+  return [0, 0]
 }
 
 console.log(twoSum([3, 4, 5, 6], 7))
