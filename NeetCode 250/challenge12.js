24cf352f550d7bd5fae13ca1b8088ba1319cab9b
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-01-27 23:15:42 -0800

    continue challenge 12

--- NeetCode 250/challenge12.js
+++ NeetCode 250/challenge12.js
@@ -8,11 +8,23 @@ function isValid(s) {
   }
 
   for (let k in sarray) {
-    if (hmap[sarray[k]] && sarray[k - 1] === hmap[sarray[k]]) stack.pop()
-    else if (hmap[sarray[k]]) return false
-    else stack.push(sarray[k])
+    if (sarray[sarray.length - 1] === hmap[sarray[k]]) {
+      stack.pop()
+      console.log(sarray[sarray.length - 1])
+      console.log('if')
+      console.log(stack)
+    } else if (hmap[sarray[k]]) {
+      console.log('else if')
+      console.log(stack)
+    } else {
+      console.log('else')
+      stack.push(sarray[k])
+      console.log(stack)
+    }
   }
+  if (stack.length === 0) return true
 }
 
 console.log(isValid('([{}])'))
+console.log('-----Example 2-----')
 console.log(isValid('(([{}])'))
