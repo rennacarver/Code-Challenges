47ec11ee8e6eccab1022566a9b8f6ebc7f39ded3
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-01-29 11:17:58 -0800

    challenge 12 complete

--- NeetCode 250/challenge12.js
+++ NeetCode 250/challenge12.js
@@ -1,24 +1,5 @@
-function isValid(s) {
-  let stack = []
-  let sarray = s.split('')
-  let hmap = {
-    '}': '{',
-    ']': '[',
-    ')': '(',
-  }
-
-  for (let k in sarray) {
-    if (hmap[sarray[k]] === undefined) stack.push(sarray[k])
-
-    if (stack.length !== 0) {
-      if (hmap[sarray[k]] && hmap[sarray[k]] === stack[stack.length - 1])
-        stack.pop()
-      else return false
-    }
-  }
-  console.log(stack)
-}
-
 console.log(isValid('([{}])'))
 console.log('-----Example 2-----')
 console.log(isValid('(([{}])'))
+console.log('-----Example 3-----')
+console.log(isValid('}(([{}])'))
