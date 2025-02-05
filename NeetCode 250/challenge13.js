74a29800b8bb7bf6d8979668527d38b9ebfdd7ea
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-02-04 21:56:43 -0800

    challenge 13 attempt 2

--- NeetCode 250/challenge13.js
+++ NeetCode 250/challenge13.js
@@ -2,13 +2,22 @@ function search(nums, target) {
   let l = 0
   let r = nums.length - 1
 
-  while (l < r - 1) {
-    l = Math.floor((l + r) / 2)
-    console.log(`nums[${l}] = ${nums[l]}`)
+  while (l <= r) {
+    let m = Math.floor((l + r) / 2)
+
+    console.log(`nums[${l}] = ${nums[l]}, nums[${r}] = ${nums[r]}`)
+
+    if (target < nums[m]) r = m
+    else if (target > nums[m]) l = m
+    else return m
+
+    console.log(`nums[${l}] = ${nums[l]}, nums[${r}] = ${nums[r]}`)
   }
-  return 0
+  return -1
 }
 
 console.log(search([-1, 0, 2, 4, 6, 8], 4))
 console.log('===test 2===')
-console.log(search([-1, 0, 2, 4, 6, 8], 4))
+console.log(search([-1, 0, 2, 4, 6, 8], 9))
+console.log('===test 3===')
+console.log(search([-1, 0, 3, 5, 9, 12], 9))
