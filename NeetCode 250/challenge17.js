69031e522d620d51293428679937e56f8f1f6f4c
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-02-25 22:39:32 -0800

    complete challenge 17

--- NeetCode 250/challenge17.js
+++ NeetCode 250/challenge17.js
@@ -15,17 +15,21 @@ class Solution {
    * @return {number}
    */
   diameterOfBinaryTree(root) {
-    let max = 0
+    //arrays are passed by reference creating a global variable
+    let max = [0]
     this.dfs(root, max)
+    return max[0]
   }
 
   dfs(root, max) {
     if (!root) return 0
 
-    let l = this.diameterOfBinaryTree(root.left)
-    let r = this.diameterOfBinaryTree(root.right)
+    let l = this.dfs(root.left, max)
+    let r = this.dfs(root.right, max)
 
     console.log(`diameter: ${l + r}`)
+
+    if (l + r > max[0]) max[0] = l + r
 
     if (r > l) return r + 1
     else return l + 1
