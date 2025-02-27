1c5c8214b71ec8724edc6cef9cf3517f8a4d9380
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-02-26 22:14:57 -0800

    learn about heaps

---
+++ NeetCode 250/challenge18.js
@@ -0,0 +1,13 @@
+class KthLargest {
+  /**
+   * @param {number} k
+   * @param {number[]} nums
+   */
+  constructor(k, nums) {}
+
+  /**
+   * @param {number} val
+   * @return {number}
+   */
+  add(val) {}
+}
