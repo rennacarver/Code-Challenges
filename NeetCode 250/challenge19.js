dd00a5900120411dd4224a2e7b63f0adad560149
Renna Carver <87547832+rennacarver@users.noreply.github.com>
2025-02-27 23:16:21 -0800

    scaffold binary tree class structure

--- NeetCode 250/challenge19.js
+++ NeetCode 250/challenge19.js
@@ -1,1 +1,16 @@
 //build a binary tree from a given array
+
+//Definition for a binary tree node.
+class TreeNode {
+  constructor(val = 0, left = null, right = null) {
+    this.val = val
+    this.left = left
+    this.right = right
+  }
+
+  insert(val) {}
+
+  delete(val) {}
+
+  search(val) {}
+}
